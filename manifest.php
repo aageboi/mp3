76721f84bf8680602a4bca394bad9eb315631155
Heri Gunawan <aageboi@gmail.com>
2022-06-16 22:55:53 +0700

    add: generator rss

---
+++ manifest.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr = [
+	[
+		'title' => 'Al Ma\'tsurat: Dzikir Pagi',
+		'description' => 'Dzikir Pagi Sesuai Sunnah Pembuka Pintu Rezeki, Kesehatan, Keamanan dan Dimudahkan Segala Urusan',
+		'url' => 'https://github.com/aageboi/mp3/blob/master/dzikir-pagi.mp3?raw=true',
+		'author' => 'Youtube Channel Yufid.tv',
+		'filename' => 'dzikir-pagi.mp3'
+	]
+];
+
+echo json_encode($arr);
